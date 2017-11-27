201c50ccdcb4c329cc9acf521a0ec35665254d5c
tanya <tatiannesterova@gmail.com>
2017-11-27 13:42:33 +0500

    fix

--- async.js
+++ async.js
@@ -3,29 +3,11 @@
 exports.isStar = true;
 exports.runParallel = runParallel;
 
-function cutJobs(jobs, parallelNum) {
-	let result = [];
-	let jobsCopy = jobs.slice();
-
-	while (jobsCopy.length > 0) {
-		result.push(jobsCopy.slice(0, parallelNum))
-		jobsCopy = jobsCopy.splice(parallelNum);
-	}
-
-	return result;
-}
-
 function getPromise(job, timeout) {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => reject(new Error(`Promise timeout`)), timeout);
 		job().then(resolve, reject);
 	});
-}
-
-function handler(translates, data, ind, amountJobs) {
-	translates.push({ ind: ind, data: data });
-
-	return translates.length == amountJobs;
 }
 
 function getListTranslates(translates) {
@@ -36,9 +18,22 @@ function getListTranslates(translates) {
 	}, [])
 };
 
-function runParallelAll(promises, parallelNum) {
+/** Функция паралелльно запускает указанное число промисов
+ * @param {Array} jobs – функции, которые возвращают промисы
+ * @param {Number} parallelNum - число одновременно исполняющихся промисов
+ * @param {Number} timeout - таймаут работы промиса
+ */
+
+function runParallel(jobs, parallelNum, timeout = 1000) {
+	if (jobs.length === 0) {
+		return Promise.resolve([]);
+	}
+	const promises = jobs.map((job, ind) => {return { ind: ind, promise: getPromise(job, timeout) }});
+
 	let translates = [];
+
 	const amountJobs = promises.length
+	
 	const startingSeries = promises.splice(0, parallelNum);
 
 	const handler = function (data, ind, resolve) {
@@ -62,19 +57,4 @@ function runParallelAll(promises, parallelNum) {
 			        err => handler(err, promise.ind, resolve));
 	    	    });
 	    	});
-}
-
-/** Функция паралелльно запускает указанное число промисов
- * @param {Array} jobs – функции, которые возвращают промисы
- * @param {Number} parallelNum - число одновременно исполняющихся промисов
- * @param {Number} timeout - таймаут работы промиса
- */
-
-function runParallel(jobs, parallelNum, timeout = 1000) {
-	const promises = jobs.map((job, ind) => {return { ind: ind, promise: getPromise(job, timeout) }})
-	//const queueRun = cutJobs(promises, parallelNum);
-
-	//let promise = Promise.resolve([]);
-
-    return (jobs.length === 0) ? Promise.resolve([]) : runParallelAll(promises, parallelNum);
 }
